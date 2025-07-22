8dc8063ae787bf2e4b34e5053a55e008b12f1ed6
Z.H. <galaxyshih@gmail.com>
2025-07-23 00:32:44 +0800

    Init cargo project

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(|| async { "IoT Monitoring Platform API" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::info!("Listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
